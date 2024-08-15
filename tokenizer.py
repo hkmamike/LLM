6c36d47e2b5139d949aae02949d70e35643f275f
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-15 01:12:45 -0700

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -7,6 +7,8 @@ preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item for item in preprocessed if item.strip()]
 
 all_words = sorted(set(preprocessed))
-vocab_size = len(all_words)
+vocab = {token:i for i,token in enumerate(all_words)}
 
-print(vocab_size)
+for i, item in enumerate(vocab.items()):
+    print(item)
+    if i > 50: break
