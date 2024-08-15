bba650650348e735353bbda67654472d54d15739
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-15 01:21:33 -0700

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -9,6 +9,18 @@ preprocessed = [item for item in preprocessed if item.strip()]
 all_words = sorted(set(preprocessed))
 vocab = {token:i for i,token in enumerate(all_words)}
 
-for i, item in enumerate(vocab.items()):
-    print(item)
-    if i > 50: break
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+    def encode(self, text) -> list:
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids):
+        text = "".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
