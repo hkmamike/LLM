d4936305050d0ee389becc3f6b2785675637d3fe
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-16 00:59:40 -0700

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -10,7 +10,7 @@ enc_text = tokenizer.encode(raw_text)
 enc_sample = enc_text[50:]
 
 context_size = 4
-x = enc_sample[:context_size]
-y = enc_sample[1:context_size+1]
-print(f"x: {x}")
-print(f"y:      {y}")
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
