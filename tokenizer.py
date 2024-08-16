c79f2b7feee37974c91d374155bca8c1589a0d11
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-16 00:17:24 -0700

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -5,17 +5,20 @@ with open("TrainningData/the-verdict.txt", "r", encoding="utf-8") as f:
 
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item for item in preprocessed if item.strip()]
-all_words = sorted(set(preprocessed))
-vocab = {token:i for i,token in enumerate(all_words)}
+all_tokens = sorted(set(preprocessed))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:i for i,token in enumerate(all_tokens)}
 
 class SimpleTokenizerV1:
     def __init__(self, vocab):
         self.str_to_int = vocab
         self.int_to_str = {i:s for s,i in vocab.items()}
 
+
     def encode(self, text) -> list:
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
     
@@ -23,9 +26,11 @@ class SimpleTokenizerV1:
         text = " ".join([self.int_to_str[i] for i in ids])
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
         return text
-    
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+
 tokenizer = SimpleTokenizerV1(vocab)
-text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable pride."""
-ids = tokenizer.encode(text)
-print(ids)
-print(tokenizer.decode(ids))
+print(tokenizer.encode(text))
+print(tokenizer.decode(tokenizer.encode(text)))
