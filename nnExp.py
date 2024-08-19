20286cb3989c5b0d08ccae0412f2de5b64e73f49
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-18 22:50:05 -0700

    Update nnExp.py

--- nnExp.py
+++ nnExp.py
@@ -21,4 +21,14 @@ class NeuralNetwork(torch.nn.Module):
     def forward(self, x):
         logits = self.layers(x)
         return logits
- 
+
+# torch.manual_seed(123) 
+model = NeuralNetwork(50, 3)
+print(model)
+
+num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print("Total number of trainable model parameters:", num_params)
+
+print(model.layers[0].weight)
+print(model.layers[0].weight.shape)
+print(model.layers[0].bias)
