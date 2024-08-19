238591f5e1024f6b86ed2192328f6942032aa849
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-18 21:11:51 -0700

    Update tensorExp.py

--- tensorExp.py
+++ tensorExp.py
@@ -11,8 +11,6 @@ a = torch.sigmoid(z)
  
 loss = F.binary_cross_entropy(a, y)
 
-grad_L_w1 = grad(loss, w1, retain_graph=True)
-grad_L_b = grad(loss, b, retain_graph=True)
-
-print(grad_L_w1)
-print(grad_L_b)
+loss.backward()
+print(w1.grad)
+print(b.grad)
