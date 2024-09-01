63440e284ef54a8bcfd441f1d0bf5339de3a1650
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-01 14:00:48 -0700

    organization

--- BasicUtils/layerNormalizationExp.py
+++ BasicUtils/layerNormalizationExp.py
@@ -39,25 +39,3 @@ class LayerNorm(nn.Module):
 # var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 # print("Mean:\n", mean)
 # print("Variance:\n", var)
-
-class GELU(nn.Module):
-    def __init__(self):
-        super().__init__()
- 
-    def forward(self, x):
-        return 0.5 * x * (1 + torch.tanh(
-            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
-            (x + 0.044715 * torch.pow(x, 3))
-        ))
-    
-class FeedForward(nn.Module):
-    def __init__(self, cfg):
-        super().__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
-            GELU(),
-            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
-        )
- 
-    def forward(self, x):
-        return self.layers(x)
--- transformer.py
+++ transformer.py
@@ -49,20 +49,48 @@ class DummyLayerNorm(nn.Module):
     def forward(self, x):
         return x
     
-import tiktoken
+# import tiktoken
  
-tokenizer = tiktoken.get_encoding("gpt2")
-batch = []
-txt1 = "Every effort moves you"
-txt2 = "Every day holds a"
+# tokenizer = tiktoken.get_encoding("gpt2")
+# batch = []
+# txt1 = "Every effort moves you"
+# txt2 = "Every day holds a"
  
-batch.append(torch.tensor(tokenizer.encode(txt1)))
-batch.append(torch.tensor(tokenizer.encode(txt2)))
-batch = torch.stack(batch, dim=0)
-print(batch)
+# batch.append(torch.tensor(tokenizer.encode(txt1)))
+# batch.append(torch.tensor(tokenizer.encode(txt2)))
+# batch = torch.stack(batch, dim=0)
+# print(batch)
 
-torch.manual_seed(123)
-model = DummyGPTModel(GPT_CONFIG_124M)
-logits = model(batch)
-print("Output shape:", logits.shape)
-print(logits)
+# torch.manual_seed(123)
+# model = DummyGPTModel(GPT_CONFIG_124M)
+# logits = model(batch)
+# print("Output shape:", logits.shape)
+# print(logits)
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+ 
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+    
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+ 
+    def forward(self, x):
+        return self.layers(x)
+    
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768)
+out = ffn(x)
+print(out.shape)
