577282de71cecdea37e51abaa1a357d28aacc925
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-08-31 22:45:56 -0700

    Update selfAttention.py

--- selfAttention.py
+++ selfAttention.py
@@ -18,3 +18,20 @@ class SelfAttention_v1(nn.Module):
             attn_scores / keys.shape[-1]**0.5, dim=-1)
         context_vec = attn_weights @ values
         return context_vec
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+
+x_2 = inputs[1]
+d_in = inputs.shape[1] # input embedding size, 3
+d_out = 2 # output size, 2. In real models, this is usually the same as input size
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
