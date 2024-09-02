c70000f64cae3b52c144e36eaecb11c6a9d5e4db
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-02 00:36:17 -0700

    Update trainning.py

--- trainning.py
+++ trainning.py
@@ -17,9 +17,9 @@ GPT_CONFIG_124M = {
     "drop_rate": 0.1,
     "qkv_bias": False
 }
-
 model = GPTModel(GPT_CONFIG_124M)
- 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def text_to_token_ids(text, tokenizer):
     encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
     encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
@@ -28,8 +28,6 @@ def text_to_token_ids(text, tokenizer):
 def token_ids_to_text(token_ids, tokenizer):
     flat = token_ids.squeeze(0) # remove batch dimension
     return tokenizer.decode(flat.tolist())
- 
-
 
 file_path = "TrainningData/the-verdict.txt"
 with open(file_path, "r", encoding="utf-8") as file:
@@ -37,8 +35,8 @@ with open(file_path, "r", encoding="utf-8") as file:
 
 total_characters = len(text_data)
 total_tokens = len(tokenizer.encode(text_data))
-print("Characters:", total_characters)
-print("Tokens:", total_tokens)
+# print("Characters:", total_characters)
+# print("Tokens:", total_tokens)
 
 train_ratio = 0.90
 split_idx = int(train_ratio * len(text_data))
@@ -64,10 +62,135 @@ val_loader = create_dataloader_v1(
     num_workers=0
 )
 
-print("Train loader:")
-for x, y in train_loader:
-    print(x.shape, y.shape)
+# print("Train loader:")
+# for x, y in train_loader:
+#     print(x.shape, y.shape)
  
-print("\nValidation loader:")
-for x, y in val_loader:
-    print(x.shape, y.shape)
+# print("\nValidation loader:")
+# for x, y in val_loader:
+#     print(x.shape, y.shape)
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1), target_batch.flatten()
+    )
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model.to(device)
+# with torch.no_grad():
+#     train_loss = calc_loss_loader(train_loader, model, device)
+#     val_loss = calc_loss_loader(val_loader, model, device)
+# print("Training loss:", train_loss)
+# print("Validation loss:", val_loss)
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model, idx=encoded,
+            max_new_tokens=50, context_size=context_size
+        )
+        decoded_text = token_ids_to_text(token_ids, tokenizer)
+        print(decoded_text.replace("\n", " "))  # Compact print format
+    model.train()
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs,
+                       eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+ 
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+ 
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+ 
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
+    return train_losses, val_losses, track_tokens_seen
+
+# model.to(device)
+# optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
+# num_epochs = 10
+# train_losses, val_losses, tokens_seen = train_model_simple(
+#     model, train_loader, val_loader, optimizer, device,
+#     num_epochs=num_epochs, eval_freq=5, eval_iter=1,
+#     start_context="Every effort moves you", tokenizer=tokenizer
+# )
+
+# model.to("cpu")
+# model.eval()
+
+# tokenizer = tiktoken.get_encoding("gpt2")
+# token_ids = generate_text_simple(
+#     model=model,
+#     idx=text_to_token_ids("Every effort moves you", tokenizer),
+#     max_new_tokens=25,
+#     context_size=GPT_CONFIG_124M["context_length"]
+# )
+# print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+
+next_token_logits = torch.tensor(
+    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+
+top_k = 3
+top_logits, top_pos = torch.topk(next_token_logits, top_k)
+print("Top logits:", top_logits)
+print("Top positions:", top_pos)
+
+new_logits = torch.where(
+    condition=next_token_logits < top_logits[-1],
+    input=torch.tensor(float('-inf')),
+    other=next_token_logits
+)
+print(new_logits)
+topk_probas = torch.softmax(new_logits, dim=0)
+print(topk_probas)
