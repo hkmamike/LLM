67a36aa292710d149d7f41cbed1493fd18abb2a9
Brian Leung (Mike) <hkmamike@users.noreply.github.com>
2024-09-02 14:32:46 -0700

    classificationFineTuning

--- fineTuning.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("TrainningData/sms_spam_collection/SMSSpamCollection.tsv", sep="\t", header=None, names=["Label", "Text"])
-print(df)
-print(df["Label"].value_counts())
